51f05e8642f47de43dcad23fcf7400f9df0ada6e
우경훈 <khwoo@sangsang.farm>
2024-02-01 13:59:30 +0900

    feat: 기본 테스트 케이스 추가

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import App from './App';
+
+describe('App test', () => {
+    it('test app is rendered', () => {
+        render(<App />);
+    });
+});
